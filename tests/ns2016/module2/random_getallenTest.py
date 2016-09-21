7b70a8baa091477d8c8226979162088463803a5a
Jelle <Jelle>
2016-09-21 12:38:32 +0200

    renamed MijnRandomGetal -> random_getal

--- tests/ns2016/module2/random_getallenTest.py
+++ tests/ns2016/module2/random_getallenTest.py
@@ -4,21 +4,21 @@ import checkpy.assertlib as assertlib
 import sys
 
 @t.test(0)
-def correctMijnRandomGetal(test):
+def correctMijn_random(test):
 	def testMethod():
-		MijnRandomGetal = lib.getFunction("MijnRandomGetal", _fileName)
-		if not assertlib.containsOnly([MijnRandomGetal(1,1) for i in range(100)], [1]):
+		mijn_random = lib.getFunction("mijn_random", _fileName)
+		if not assertlib.containsOnly([mijn_random(1,1) for i in range(100)], [1]):
 			return False, "Huh? a random number between 1 and 1 gives something unexpected"
-		if not assertlib.containsOnly([MijnRandomGetal(0,1) for i in range(100)], [0,1]):
+		if not assertlib.containsOnly([mijn_random(0,1) for i in range(100)], [0,1]):
 			return False, "Huh? a random number between 0 and 1 can become something other than 0 or 1?!"
 		return True, ""
 	test.test = testMethod
 	
-	test.description = lambda : "MijnRandomGetal functions correctly"
+	test.description = lambda : "mijn_random functions correctly"
 	test.fail = lambda info : str(info)
 
 
-@t.passed(correctMijnRandomGetal)
+@t.passed(correctMijn_random)
 @t.test(1)
 def correctVierkant(test):
 	test.test = lambda : assertlib.between(lib.getFunction("Vierkant", _fileName)(), 0.45, 0.55)
