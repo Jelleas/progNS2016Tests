8c574e1d27a26cd5c6056ab80815b8ea17efe3e7
Jelle <Jelle>
2016-09-21 12:23:42 +0200

    renamed Nulpunten -> nulpunten

--- tests/ns2016/module2/functiesTest.py
+++ tests/ns2016/module2/functiesTest.py
@@ -6,17 +6,17 @@ import sys
 @t.test(0)
 def correct(test):
 	def testMethod():
-		points = lib.getFunction("Nulpunten", _fileName)(1,2,-10)
+		points = lib.getFunction("nulpunten", _fileName)(1,2,-10)
 		for i, point in enumerate(points):
 			points[i] = int(point * 10)
 		testResult = 23 in points and -43 in points
 		return testResult, ""
 	test.test = testMethod
 	
-	test.description = lambda : "output of Nulpunten is correct for the example a=1, b=2, c=-10"
+	test.description = lambda : "output of nulpunten is correct for the example a=1, b=2, c=-10"
 
 
 @t.test(1)
 def returnTypeIsList(test):
-	test.test = lambda : assertlib.sameType(lib.getFunction("Nulpunten", _fileName)(1,2,-10), [])
-	test.description = lambda : "correct return type of Nulpunten"
+	test.test = lambda : assertlib.sameType(lib.getFunction("nulpunten", _fileName)(1,2,-10), [])
+	test.description = lambda : "correct return type of nulpunten"
