44f94e88626c8f9aad226a927d05a3bfa10e2f17
Martijn Stegeman <martijn@stgm.nl>
2016-10-06 09:49:21 +0200

    vertaling tests + refinement

--- tests/ns2016/module3/fitTest.py
+++ tests/ns2016/module3/fitTest.py
@@ -8,9 +8,9 @@ def before():
 @t.test(0)
 def correctC(test):
 	test.test = lambda : assertlib.contains(lib.outputOf(_fileName).split("\n")[0], "60.")
-	test.description = lambda : "correct value of c"
+	test.description = lambda : "print de beste waarde van c"
 
 @t.test(1)
 def correctUncertainy(test):
 	test.test = lambda : assertlib.contains(lib.outputOf(_fileName).split("\n")[1], "1.5")
-	test.description = lambda : "correct uncertainty"
+	test.description = lambda : "print de onzekerheid bij c"
