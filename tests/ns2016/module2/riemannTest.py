2d61a54eadf81c69941798246d1268e8ec3f133a
jelleas <jelle.van.assema@gmail.com>
2016-11-18 04:42:37 +0100

    fixed functies tests

--- tests/ns2016/module2/riemannTest.py
+++ tests/ns2016/module2/riemannTest.py
@@ -21,17 +21,17 @@ def hasRiemann(test):
 @t.passed(hasRiemann)
 @t.test(1)
 def correctFunc1(test):
-	test.test = lambda : assertlib.exact(int(lib.getFunction("riemann", _fileName)(lambda x : x**(x + 0.5), 0, 1, 10000) * 100), 52)
+	test.test = lambda : assertlib.between(lib.getFunction("riemann", _fileName)(lambda x : x**(x + 0.5), 0, 1, 10000), 0.52, 0.53)
 	test.description = lambda : "riemann werkt correct voor x^(x + 0.5) van a=0 tot b=1 met n=10000"
 
 @t.passed(hasRiemann)
 @t.test(2)
 def correctFunc2(test):
-	test.test = lambda : assertlib.exact(int(lib.getFunction("riemann", _fileName)(lambda x : math.tan(math.cos(math.sin(x))), 0.2, 2.2, 10000) * 100), 85)
+	test.test = lambda : assertlib.between(lib.getFunction("riemann", _fileName)(lambda x : math.tan(math.cos(math.sin(x))), 0.2, 2.2, 10000), 1.70, 1.71)
 	test.description = lambda : "riemann werkt correct voor tan(cos(sin(x))) van a=0.2 tot b=2.2 met n=10000"
 
 @t.passed(hasRiemann)
 @t.test(3)
 def correctFunc3(test):
-	test.test = lambda : assertlib.exact(int(lib.getFunction("riemann", _fileName)(lambda x : math.sin(x**2), 0, math.pi, 10000) * 100), 24)
+	test.test = lambda : assertlib.between(lib.getFunction("riemann", _fileName)(lambda x : math.sin(x**2), 0, math.pi, 10000), 0.77, 0.78)
 	test.description = lambda : "riemann werkt correct voor sin(x^2) van a=0 tot b=pi met n=10000"
