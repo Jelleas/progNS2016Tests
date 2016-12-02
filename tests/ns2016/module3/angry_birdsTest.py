964b95df675335ef3696fd27a593ca3b948969d0
jelleas <jelle.van.assema@gmail.com>
2016-12-02 10:23:17 +0100

    allow for more missed answers in angry_birds

--- tests/ns2016/module3/angry_birdsTest.py
+++ tests/ns2016/module3/angry_birdsTest.py
@@ -22,13 +22,18 @@ def hasBeweging(test):
 def correctOutput(test):
 	def testMethod():
 		answers = [-88, -87, -86, -84, -81, -69, -68, -67, -66, -65, 15, 16, 17, 74, 75, 81, 87]
+		nAnswers = len(answers)
+		nCorrect = 0
 		for line in lib.outputOf(_fileName).split("\n"):
 			for i, answer in enumerate(answers):
 				if assertlib.contains(line, str(answer)):
 					del answers[i]
+					nCorrect += 1
 					break
+			else:
+				nCorrect -= 1
 
-		return len(answers) <= 3
+		return nAnswers - nCorrect <= 5 
 
 	test.test = testMethod
 	test.description = lambda : "print de juiste hoeken waarbij de vogel de sensor raakt"
